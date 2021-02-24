7117ed5c44497941ab9bb6c836fb73562c14892f
prateekch33 <prateekchandra27@outlook.com>
2021-02-25 01:17:14 +0530

    init

---
+++ aomatic.js
@@ -0,0 +1,6 @@
+var ans=prompt("Are we there yet");
+while(ans!=="yes"&&ans!=="yeah"&&ans!=="Yes"&&ans!=="Yeah")
+{
+    var ans=prompt("Are we there yet")    
+}
+alert("Yay, we finally made it!");
